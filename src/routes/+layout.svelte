401bfd209c507886b10f3d67b78c0c73b02ae19c
mr-wellick <lorenz3192@gmail.com>
2024-06-05 21:11:18 -0700

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Sidebar from "$lib/+sidebar.svelte";
+import "../app.css";
+</script>
+
+<div class="grid grid-cols-12">
+    <div class="col-span-2">
+        <Sidebar />
+    </div>
+
+    <div class="col-span-9">
+        <slot></slot>
+    </div>
+
+    <div class="col-span-1">
+        <slot></slot>
+    </div>
+</div>
