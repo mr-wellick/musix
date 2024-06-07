0f302a894c471b044f7015c6181ef7e874f03188
mr-wellick <lorenz3192@gmail.com>
2024-06-06 17:21:15 -0700

    clean up
    	- Refactor code to use reactivity model mindset
    	- Add dayjs package
    	- Added calendar & timeline components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,17 @@
 <script>
-  import Sidebar from "$lib/+sidebar.svelte";
-import "../app.css";
+	import Calendar from '$lib/+calendar.svelte';
+	import '../app.css';
 </script>
 
 <div class="grid grid-cols-12">
-    <div class="col-span-2 justify-items-center">
-        <Sidebar />
-    </div>
+	<div class="col-span-2 justify-items-center">
+		<h1 class="p-2 text-center">Musix</h1>
+		<Calendar />
+	</div>
 
-    <div class="col-span-9">
-        <slot></slot>
-    </div>
+	<div class="col-span-9">
+		<slot></slot>
+	</div>
 
-    <div class="col-span-1">
-        Menu
-    </div>
+	<div class="col-span-1">Menu</div>
 </div>
