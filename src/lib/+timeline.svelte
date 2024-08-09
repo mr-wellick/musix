ce315c6cb02fe542ff4445e60b21cd1dc7ff4d1e
mr-wellick <lorenz3192@gmail.com>
2024-08-09 01:30:55 -0700

    fix linting errors/warnings

--- src/lib/+timeline.svelte
+++ src/lib/+timeline.svelte
@@ -25,14 +25,16 @@
 		// 	bpm: '80'
 		// }
 	];
-	let dragged: EventTarget | null;
+	let dragged: HTMLDivElement;
 	const hours = Array.from({ length: 24 }, (_, index) => index);
 </script>
 
 <div
 	class="flex relative w-full"
-	role="table"
-	on:dragover={(e) => {
+	role="button"
+    tabindex="0"
+    on:dragover={
+    (e) => {
 		e.preventDefault();
 		if (dragged) {
 			console.log('drag over');
@@ -40,37 +42,16 @@
 			dragged.style.top = e.clientY + 'px';
 		}
 	}}
-	on:dragenter={(e) => {
+    on:dragenter={() => {
 		//console.log('drag enter');
 	}}
-	on:dragleave={(e) => {
+	on:dragleave={() => {
 		//console.log('drag leave');
 	}}
 	on:drop={(e) => {
 		e.preventDefault();
 		//console.log('drop', e.target.getBoundingClientRect());
 		//e.target.appendChild(dragged);
-	}}
-	on:click={(e) => {
-		console.log('add new item: ', {
-			category: 'Scales',
-			name: 'Scale in 3rds #1',
-			description:
-				'Practice scale in 3rds ascending/descending around the circle of fifths. Use scale pattern #1 (one position, no shifting)',
-			duration: '10m',
-			bpm: '80'
-		});
-		todos = [
-			...todos,
-			{
-				category: 'Scales',
-				name: 'Scale in 3rds #1',
-				description:
-					'Practice scale in 3rds ascending/descending around the circle of fifths. Use scale pattern #1 (one position, no shifting)',
-				duration: '10m',
-				bpm: '80'
-			}
-		];
 	}}
 >
 	<ul>
@@ -84,13 +65,14 @@
 			class="absolute table cursor-pointer border border-musix-red-border rounded h-min"
 			role="contentinfo"
 			draggable="true"
-			on:drag={(e) => {
+            on:drag={() => {
 				//console.log('dragging');
 			}}
 			on:dragstart={(e) => {
-				dragged = e.target;
+                if(e.target)
+                    dragged = e.target as HTMLDivElement
 			}}
-			on:dragend={(e) => {
+            on:dragend={() => {
 				//console.log('drag end');
 			}}
 		>
