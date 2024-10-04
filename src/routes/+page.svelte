0175370c8e9ff184cdba29d3009132a6407d2250
mr-wellick <lorenz3192@gmail.com>
2024-10-03 23:13:07 -0700

    trying out svelte context

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
-  import Timeline from "$lib/+timeline.svelte";
+	import Timeline from '$lib/+timeline.svelte';
+	import { getContext } from 'svelte';
+
+	// Retrieve user store from context
+	const tasks = getContext('tasks');
 </script>
 
 <Timeline />
+
+{#each $tasks as task}
+	{JSON.stringify(task)}
+{/each}
