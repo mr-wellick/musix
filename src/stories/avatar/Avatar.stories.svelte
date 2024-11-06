8e344de6cc3e2539144f67e0c92d7575e7c36de0
mr-wellick <lorenz3192@gmail.com>
2024-11-06 00:33:59 -0800

    add daisy ui and play around with storybooks

---
+++ src/stories/avatar/Avatar.stories.svelte
@@ -0,0 +1,16 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import Avatar from './Avatar.svelte';
+
+  const { Story } = defineMeta({
+    title: 'Avatar',
+    component: Avatar,
+    tags: ['autodocs'],
+    // argTypes: {},
+    args: {
+      user: { name: 'Daffy' },
+    },
+  });
+</script>
+
+<Story name="Primary" />
---
+++ src/stories/avatar/Avatar.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  const { user }: { user: { name: string } } = $props();
+</script>
+
+<div tabindex="0" role="button" class="avatar placeholder">
+  <div class="w-12 rounded-full bg-neutral text-neutral-content">
+    <span>{user.name.at(0)}</span>
+  </div>
+</div>
