84217da13c5f2057e229d025eeab3ca0d349cf9f
mr-wellick <lorenz3192@gmail.com>
2024-11-15 03:12:58 -0800

    add footer and sidebar stories

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,8 @@
   import Header from '../stories/header/Header.svelte';
   import { userStore } from '$lib/userStore.svelte';
   import '../app.css';
+  import Sidebar from '../stories/sidebar/Sidebar.svelte';
+  import Footer from '../stories/footer/Footer.svelte';
 
   let { children } = $props();
 
@@ -11,5 +13,8 @@
 </script>
 
 <Header {onLogout} user={$userStore} />
+<Sidebar />
 
 {@render children()}
+
+<Footer />
---
+++ src/stories/sidebar/Sidebar.stories.svelte
@@ -0,0 +1,13 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import Sidebar from './Sidebar.svelte';
+
+  const { Story } = defineMeta({
+    title: 'Components/Sidebar',
+    component: Sidebar,
+    tags: ['autodocs'],
+    args: {},
+  });
+</script>
+
+<Story name="Opened" />
