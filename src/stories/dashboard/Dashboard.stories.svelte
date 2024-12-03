6ca1507c395dcdbc63e33c321e186d96642ee7f9
mr-wellick <lorenz3192@gmail.com>
2024-12-02 22:09:22 -0800

    add dashboard view
    	- work in progress

---
+++ src/stories/dashboard/Dashboard.stories.svelte
@@ -0,0 +1,13 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import Dashboard from './Dashboard.svelte';
+
+  const { Story } = defineMeta({
+    title: 'View/Dashboard',
+    component: Dashboard,
+    tags: ['autodocs'],
+    args: {},
+  });
+</script>
+
+<Story name="Logged in" />
---
+++ src/stories/dashboard/Dashboard.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Header from '../header/Header.svelte';
+  import Sidebar from '../sidebar/Sidebar.svelte';
+</script>
+
+<Header data={{ user: { first_name: 'Bugs', last_name: 'Bunny' } }} onLogout={() => {}} />
+<Sidebar />
